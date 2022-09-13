e9b5d0216a896fa65868ee275fc0c668cd72b49f
sebasperezcabanas <sebasperezcabanas@gmail.com>
2022-09-13 02:41:14 -0300

    k_means_12_09_22

--- actividad_1.py
+++ actividad_1.py
@@ -1,4 +1,3 @@
-from asyncio.windows_events import NULL
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,57 +5,70 @@ import matplotlib.pyplot as plt
 # k = cantidad de clústers
 # centros = matriz de centros de clúster
 # datos = matriz de datos
-def k_means(k, centros, datos):
-    dist_cluster = []
-    labels = []
-    centros_ant = np.array([[1,1],[1,1]]).reshape(2, 2)
-    j = 0
+def k_means(k, centres, data, dim):
+    pre_centres = np.ones(k*dim).reshape(k, dim)
+    it = 0
 
-    while (not(np.array_equal(centros, centros_ant))):
-        j += 1
-        dist_cluster.clear()
-        for i in range(datos.shape[0]):
-            dist_cluster.append(np.linalg.norm(datos[i]-centros[0]))
-            dist_cluster.append(np.linalg.norm(datos[i]-centros[1]))
-        dista = np.array(dist_cluster).reshape(32,2)
-        labels = np.argmin(dista, axis=1)
+    while (not(np.array_equal(centres, pre_centres))):
+        it += 1
+        dist_cluster = np.zeros(data.shape[0]*k).reshape(data.shape[0],k)
+        mat_pert = np.zeros((data.shape[0]*k), dtype=int).reshape(data.shape[0],k)
         
-        centros_ant = centros.copy()
-        centros = np.array([[0,0],[0,0]])
-        cant_c1 = 0
-        cant_c2 = 0
-        for i in range(labels.shape[0]):
-            if labels[i] == 0:
-                centros[0] += datos[i]
-                cant_c1 += 1
-            else:
-                centros[1] += datos[i]
-                cant_c2 += 1
+        for i in range(data.shape[0]):
+            for j in range(k):
+                dist_cluster[i][j]= np.linalg.norm(data[i]-centres[j])
+
+        print(f"dist_cluster: {dist_cluster}")
+        pre_centres = centres.copy()
+        centres = np.zeros(k*dim).reshape(k,dim)
+        amount_centres = np.zeros((k), dtype=int)
+        aux = np.argmin(dist_cluster, axis=1)
         
-        if cant_c1 != 0:
-            centros[0] = centros[0]/cant_c1
-        if cant_c2 != 0:
-            centros[1] = centros[1]/cant_c2
+        print(f"aux: {aux}")
+
+        for i in range(aux.shape[0]):
+            mat_pert[i][aux[i]] = 1
+
+        print(f"mat_pert: {mat_pert}")
+
+        for j in range(mat_pert.shape[1]):
+            for i in range(mat_pert.shape[0]):
+                if mat_pert[i][j] == 1:
+                    centres[j] += data[i]
+                    amount_centres[j] += 1 
+        
+        for i in range(amount_centres.shape[0]):
+            if amount_centres[i] != 0:
+                centres[i] = centres[i]/amount_centres[i]
+        
+        print(f"--------------------- ITER: {it}")
+        print(f"cant_centros: {amount_centres}")
+        print(f"centros: {centres}")
+        print(f"centros_ant: {pre_centres}")
     
-    return dista, labels, centros, j
+    return dist_cluster, centres, it
+
+#Función para generar datos
+def data_generate():
+    
+    data_1 = np.random.randint(low=0,high=35,size=32).reshape(16,2)
+    data_2 = np.random.randint(low=25,high=50,size=32).reshape(16,2)
+    
+    return np.concatenate((data_1,data_2))
 
 
-datos_1 = np.random.randint(low=0,high=35,size=32).reshape(16,2)
-datos_2 = np.random.randint(low=25,high=50,size=32).reshape(16,2)
-datos_total = np.concatenate((datos_1,datos_2))
+total_data = data_generate()
+print(f"Datos totales:\n {total_data}")
 
-print("Datos totales:\n", datos_total)
-
-centrosIni = np.array([[0,0],[0,0]]).reshape(2, 2)
-distancias, labels, centros, iteraciones = k_means(2, centrosIni, datos_total)
+init_centres = np.zeros(4).reshape(2, 2)
+distancias, centros, iteraciones = k_means(2, init_centres, total_data, 2)
 
 print("\nDistancias al centro de cluster:\n", distancias)
 print("\nCentros de cluster:\n", centros)
-print("\nLabels:\n", labels)
 print(f'\nIteraciones: {iteraciones}')
 
 #----------------------------- GRAFICO -----------------------------
-
+"""
 X1 = []; X2 = []; Y1 = []; Y2 = []
 for i in range(labels.shape[0]):
     if labels[i] == 0:
@@ -75,3 +87,4 @@ plt.plot(X2, Y2, color='blue', marker='o', linestyle='')
 plt.plot(W, Z, 'r+')
 plt.title("Gráfico")
 plt.show()
+"""
